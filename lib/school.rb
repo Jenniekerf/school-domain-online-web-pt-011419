5bdb0395f7e916e348209dea62f95fae1d0efb5a
Jennie Kerfstedt <jenniekerf@gmail.com>
2019-03-07 15:59:42 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,28 +1,26 @@
 class School
   
- attr_reader :name, :roster
- 
+  attr_accessor :roster, :name
+  
   def initialize(name)
-    @name = name 
-    @roster = {}
-  end
-  
-  
-  def add_student(student,grade)
-    roster[grade] ||= []
-    roster[grade] << student
-  end
-  
-  def grade(student_grade)
-    roster[student_grade]
-    end 
-  
-  def sort
-    sorted = {} 
-    roster.each do |grade, student|
-      sorted[grade] = student.sort
-    end
-    sorted
-  end 
+  @roster = {}
+  @name = name
+end 
 
+def add_student(student, grade)
+  roster[grade] ||= []
+  roster[grade] << student
+end
+
+def grade(grade)
+  roster[grade]
+end
+
+def sort
+  sorted = {}
+  roster.each do |grade, student|
+    sorted[grade] = student.sort 
+  end
+  sorted
+end
 end
